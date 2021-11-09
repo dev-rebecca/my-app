a0baa06af8e475f0586f6314b2e9bd4301e3aaca
Rebecca <rebeccakatemansell@gmail.com>
2021-11-09 10:35:13 +1000

    admin panel returning list of species!

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
 import React, { useState } from "react";
-import Species from "./components/Species/Species";
-import NewSpecies from "./components/NewSpecies/NewSpecies";
+import SpeciesInfo from "./practice";
+// import Species from "./components/Species/Species";
+// import NewSpecies from "./components/NewSpecies/NewSpecies";
 
-const DUMMY_DATA = [
-  {
-    id: "s1",
-    name: "Koala",
-    status: "Approved",
-    date: new Date(2020, 9, 17),
-  },
-  {
-    id: "s1",
-    name: "Wallaby",
-    status: "Approved",
-    date: new Date(2021, 2, 16),
-  },
-  {
-    id: "s1",
-    name: "Blue Tongue Lizard",
-    status: "Pending",
-    date: new Date(2021, 5, 14),
-  },
-  {
-    id: "s1",
-    name: "Brisbane River Turtle",
-    status: "Pending",
-    date: new Date(2020, 3, 24),
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: "s1",
+//     name: "Koala",
+//     status: "Approved",
+//     date: new Date(2020, 9, 17),
+//   },
+//   {
+//     id: "s1",
+//     name: "Wallaby",
+//     status: "Approved",
+//     date: new Date(2021, 2, 16),
+//   },
+//   {
+//     id: "s1",
+//     name: "Blue Tongue Lizard",
+//     status: "Pending",
+//     date: new Date(2021, 5, 14),
+//   },
+//   {
+//     id: "s1",
+//     name: "Brisbane River Turtle",
+//     status: "Pending",
+//     date: new Date(2020, 3, 24),
+//   },
+// ];
 
 const App = () => {
 
-  const [species, setSpecies] = useState(DUMMY_DATA);
+  // const [species, setSpecies] = useState(DUMMY_DATA);
 
-  const addSpeciesHandler = (species) => {
-    setSpecies((prevSpecies) => {
-      return [species, ...prevSpecies];
-    });
-  };
+  // const addSpeciesHandler = (species) => {
+  //   setSpecies((prevSpecies) => {
+  //     return [species, ...prevSpecies];
+  //   });
+  // };
 
   return (
     <div>
-      <NewSpecies onAddSpecies={addSpeciesHandler} />
-      <Species animals={species} />
+      {/* <NewSpecies onAddSpecies={addSpeciesHandler} />
+      <Species animals={species} /> */}
+      <SpeciesInfo />
     </div>
   );
 };
--- src/practice.js
+++ src/practice.js
@@ -1,48 +1,45 @@
-// import './App.css';
-// import React from 'react';
+import "./App.css";
+import React from "react";
 
-// export default class SpeciesInfo extends React.Component {
+export default class SpeciesInfo extends React.Component {
+  state = {
+    loading: true,
+    species: null,
+  };
 
-//     state = {
-//         loading: true,
-//         species: null
-//     }
-   
-//     async componentDidMount() {
-//         const url = "ws.php?page=view-all-species";
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         this.setState({ species: data, loading: false });
-//     }
+  async componentDidMount() {
+    const url = "ws.php?page=view-all-species";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ species: data, loading: false });
+  }
 
-//     render() {
-//         if (this.state.loading) {
-//             return <div>Loading...</div>;
-//         }
+  render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    }
 
-//         if (!this.state.species) {
-//             return <div>Didn't get any species info</div>;
-//         }
+    if (!this.state.species) {
+      return <div>Didn't get any species info</div>;
+    }
 
-//         return (
-//             <div>
-//                 <table class="table-auto">
-//                 <thead>
-//                 <tr>
-//                     <th>Species</th>
-//                     <th>Date</th>
-//                     <th>Status</th>
-//                 </tr>
-//                 </thead>
-//                 <tbody>
-//                 <tr>
-//                     <td>{this.state.species.name}</td>
-//                     <td>{this.state.species.date}</td>
-//                     <td>{this.state.species.status}</td>
-//                 </tr>
-//                 </tbody>
-//             </table>
-//           </div>
-//         );
-//     }   
-// }
+    return (
+      <div>
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Species</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{this.state.species.map((item, i) => <p key={i}>{item.name}</p>)}</td>
+              <td>{this.state.species.map((item, i) => <p key={i}>{item.status}</p>)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
